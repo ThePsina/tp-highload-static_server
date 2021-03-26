d255727c9cc46337efbe19070a494b72eb05ac1a
thePsina <mishele20@mail.ru>
2021-03-26 17:11:27 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ int main() {
                 break;
             } else if (pid < 0) {
                 std::cout << "bad fork " << std::endl;
+                return -2;
             }
         }
 
